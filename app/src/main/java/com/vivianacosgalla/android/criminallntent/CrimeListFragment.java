93fa21cda31248a90a8e57ccbda60b5ec7edaf56
Cinthia cosgalla <13470206@itcampeche.edu.mx>
2017-12-05 12:44:50 -0600

    criminallntent: Muestra una linea personalizada de crimenes

--- app/src/main/java/com/vivianacosgalla/android/criminallntent/CrimeListFragment.java
+++ app/src/main/java/com/vivianacosgalla/android/criminallntent/CrimeListFragment.java
@@ -9,7 +9,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -44,14 +46,40 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
-        public TextView mTitleTextView;
+    private class CrimeHolder extends RecyclerView.ViewHolder{
+    /**implements View.OnClickListener {}*/
+        private Crime mCrime;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckBox;
+
 
         public CrimeHolder(View itemView) {
             super(itemView);
+            /**itemView.setOnClickListener(this);*/
 
-            mTitleTextView = (TextView) itemView;
+            mTitleTextView = (TextView) itemView
+            .findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView=(TextView)itemView
+                    .findViewById(R.id.list_item_crime_date_text_view);
+            mSolvedCheckBox =(CheckBox)itemView
+                    .findViewById(R.id.list_item_crime_solved_check_box);
+
+
         }
+        public void bindCrime(Crime crime){
+            mCrime= crime;
+            mTitleTextView.setText(mCrime.getmTitle());
+            mDateTextView.setText(mCrime.getmDate().toString());
+            mSolvedCheckBox.setChecked(mCrime.ismSolved());
+        }
+        /**@Override
+        public void onClick(View view){
+            Toast.makeText(getActivity(),
+                    "Se hizo clic sobre" + mCrime.getmTitle(),
+                    Toast.LENGTH_SHORT).show();
+
+        }*/
     }
 
         private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
@@ -64,16 +92,15 @@ public class CrimeListFragment extends Fragment {
             @Override
             public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType){
                 LayoutInflater layoutInflater=LayoutInflater.from(getActivity());
-                View view = layoutInflater.inflate(
-                        android.R.layout.simple_list_item_1,
-                        parent, false);
+                View view = layoutInflater.inflate(R.layout.list_item_crime, parent, false);
                 return new CrimeHolder(view);
             }
 
             @Override
             public void onBindViewHolder(CrimeHolder holder, int position){
                 Crime crime=mCrimes.get(position);
-                holder.mTitleTextView.setText(crime.getmTitle());
+                holder.bindCrime(crime);
+
             }
 
             @Override
